f7e733c374f19618d185560f9b973ad9a244e659
tanimuranaomichi <tanimuranaomichi@gmail.com>
2021-05-31 19:16:18 +0900

    ストップウォッチ機能削除

--- app/src/main/java/com/example/hikikomori/MainActivity.kt
+++ app/src/main/java/com/example/hikikomori/MainActivity.kt
@@ -21,18 +21,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val totalTime = findViewById<TextView>(R.id.totalTime)
-        val runnable = object : Runnable {
-            override fun run() {
-                timeValue++
-
-                timeToText(timeValue)?.let {
-                    totalTime.text = it
-                }
-                handler.postDelayed(this, 1000)
-            }
-        }
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
                 checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
@@ -51,22 +39,9 @@ class MainActivity : AppCompatActivity() {
 
         val ssidChecker = SSIDChecker(this)
         if (ssidChecker.checkSSID("ogatalab")) {
-            handler.post(runnable)
         }
     }
-
-    private fun timeToText(time: Int = 0): String? {
-        return if (time < 0) {
-            null
-        } else if (time == 0) {
-            "00:00:00"
-        } else {
-            val h = time / 3600
-            val m = time % 3600 / 60
-            val s = time % 60
-            "%1$02d:%2$02d:%3$02d".format(h, m, s)
-        }
-    }
+    
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
